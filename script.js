b16a052216c8be6b0ea526cce40346d4d0feda48
OtradinskiyIvan <ivanotradinskij@gmail.com>
2026-03-14 15:28:21 +0300

    initial js and added buttons for basket

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button1 = document.getElementById("btn1")
+    const button2 = document.getElementById("btn2")
+    const button3 = document.getElementById("btn3")
+
+    button1.addEventListener("click", function() {
+        alert("Товар добавлен в корзину!")
+    })
+
+    button2.addEventListener("click", function() {
+        alert("Товар добавлен в корзину!")
+    })
+
+    button3.addEventListener("click", function() {
+        alert("Товар добавлен в корзину!")
+    })
+
+})
